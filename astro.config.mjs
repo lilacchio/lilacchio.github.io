891c2ecc22c4d3f534d11ad4c565f2df718ad2ea
S <sakil.m880@gmail.com>
2022-06-26 03:53:48 -0600

    first
    first

---
+++ astro.config.mjs
@@ -0,0 +1,15 @@
+import { defineConfig } from 'astro/config';
+import preact from '@astrojs/preact';
+
+import react from "@astrojs/react";
+import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [preact(), react(), tailwind(), sitemap()]
+});
+
+{
+  site: 'https://darkrider1999.github.io'
+}
